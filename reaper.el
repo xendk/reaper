5955910b3ad81bdfc566487e662ed2a7e50ae4d9
Andrey Koppel <akoppela@gmail.com>
2019-09-10 04:07:44 +0800

    Added custom variable to switch between Harvest time modes.

--- reaper.el
+++ reaper.el
@@ -49,6 +49,12 @@
   :type 'string
   :group 'reaper)
 
+(defcustom reaper-hours-timer-mode t
+  "Defines if Harvest is configured to track time via duration.
+   Non-t value means Harvest is configured to track time via start and end time."
+  :type 'boolean
+  :group 'reaper)
+
 (defconst reaper--list-format
   [("Project" 32 nil)
    ("Task" 20 nil)
@@ -417,24 +423,14 @@ Stops any previously running timers."
          (entry (assoc entry-id reaper-timeentries))
          ;; If the timer is running add the time since the data was fetched.
          (time (reaper--hours-to-time (reaper--hours-accounting-for-running-timer entry)))
-         (new-time (reaper--time-to-hours-calculation (read-string "New time: " time)))
-         (harvest-payload (make-hash-table :test 'equal)))
-    (puthash "hours" new-time harvest-payload)
-    (reaper-api "PATCH" (format "time_entries/%s" entry-id) harvest-payload "Updated entry")
-    (reaper-refresh)))
-
-(defun reaper-edit-entry-start-end-time ()
-  "Edit start/end time of entry at point."
-  (interactive)
-  (let* ((entry-id (tabulated-list-get-id))
-         (entry (assoc entry-id reaper-timeentries))
          (start-time (cdr (assoc :started_time entry)))
-         (new-start-time (read-string "New start time: " start-time))
          (end-time (cdr (assoc :ended_time entry)))
-         (new-end-time (read-string "New end time: " end-time))
          (harvest-payload (make-hash-table :test 'equal)))
-    (puthash "started_time" new-start-time harvest-payload)
-    (puthash "ended_time" new-end-time harvest-payload)
+    (if reaper-hours-timer-mode
+        (puthash "hours" (reaper--time-to-hours-calculation (read-string "New time: " time)) harvest-payload)
+      (progn
+        (puthash "started_time" (read-string "New start time: " start-time) harvest-payload)
+        (puthash "ended_time" (read-string "New end time: " end-time) harvest-payload)))
     (reaper-api "PATCH" (format "time_entries/%s" entry-id) harvest-payload "Updated entry")
     (reaper-refresh)))
 
