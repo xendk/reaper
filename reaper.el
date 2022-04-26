2cfe54e9f5470415ab5f59d3c0829bfce41525ad
Thomas Fini Hansen <xen@xen.dk>
2022-04-26 22:48:36 +0200

    Require newer Emacs version
    Needed for `assoc-delete-all`.

--- reaper.el
+++ reaper.el
@@ -5,7 +5,7 @@
 ;; Author: Thomas Fini Hansen <xen@xen.dk>
 ;; Created: August 11, 2019
 ;; Version: 1.0.0
-;; Package-Requires: ((emacs "25.1"))
+;; Package-Requires: ((emacs "26.2"))
 ;; Keywords: tools
 ;; Url: https://github.com/xendk/reaper
 
